f72ca6f93e6dba263b2a82b7df7b0d5b93eae7cb
pavel.butuzov <pavel.butuzov@testit.software>
2026-07-20 15:43:46 +0300

    fix: TMS-40498: fix the WorkItemApiResult model.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "testit-api-client"
 
-VERSION = "7.6.0.post580"
+VERSION = "7.6.1.post580"
 
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
